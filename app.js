f54aee05361f5124151c034ac692410cd57d6b94
Sarah Wooten <snwooten@gmail.com>
2018-01-15 18:32:06 -0500

    completed logging middleware

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require ('express');
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url, res.statusCode);
+  next();
+});
+
 app.get('/', (req, res) =>
         res.send('howdy'));
 
+app.get('/news', (req, res) =>
+        res.send('this is the evening news'));
 
 
 
